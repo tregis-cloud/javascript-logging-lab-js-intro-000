45ba4597e37e43b84dd3a79b510aa04e79c04812
Terrence Regis <terrence.regis@gmail.com>
2020-04-09 17:44:34 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -8,9 +8,9 @@ describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
 
-  it('calls console.error("HALP!")', done => {
+  it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
-
+console.error("HALP!")
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }, (err, window) => {
